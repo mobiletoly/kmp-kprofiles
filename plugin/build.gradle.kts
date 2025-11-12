2a6bae0c653df41a68fea6d09bbeebdf91710f38
Toly Pochkin <mr.toly@gmail.com>
2025-11-12 17:39:56 +0000

    initial release

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "dev.goquick.kprofiles"
-version = "0.1.0-SNAPSHOT"
+version = "0.1.1"
 
 repositories {
     mavenCentral()
@@ -17,7 +17,6 @@ dependencies {
     implementation(gradleApi())
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin-api:2.2.21")
     compileOnly("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.21")
-    runtimeOnly("org.jetbrains.kotlin:kotlin-gradle-plugin:2.2.21")
     compileOnly("org.jetbrains.compose:compose-gradle-plugin:1.9.3")
     implementation("org.yaml:snakeyaml:2.2")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.17.1")
@@ -30,6 +29,8 @@ gradlePlugin {
     plugins {
         create("kprofilesPlugin") {
             id = "dev.goquick.kprofiles"
+            displayName = "KProfiles Resource Overlay Plugin"
+            description = "Kprofiles plugin to overlay Compose resources"
             implementationClass = "dev.goquick.kprofiles.KprofilesPlugin"
         }
     }
